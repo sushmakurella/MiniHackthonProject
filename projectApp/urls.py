090a034616ba5ee8eea0b5e0b047542fd0636d3a
sushmakurella <suahmakurella@gmail.com>
2024-10-25 12:56:13 +0530

    f1

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns=[
     path('',views.home,name='home')
 ]
+urlpatterns += staticfiles_urlpatterns()
--- projectMiniHackthon/settings.py
+++ projectMiniHackthon/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'projectMiniHackthon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.parse("postgresql://projectminihackthon_user:gRLzy1kEHUyP3B1UZoX7poZJt4fl5kf9@dpg-csdk74pu0jms73a4pkd0-a.oregon-postgres.render.com/projectminihackthon")
 }
 
 
@@ -114,9 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
